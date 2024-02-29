ab3b74c0a686c1945ce38c3c9bb28d9fcbc480b0
Alessadro <alessandro.ferreira@compasso.com.br>
2024-02-29 10:37:40 -0300

    Refactoring

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ import startGrpcServer from './src/main/grpc/grpcServer.js';
 import startHttpProxy from './src/main/http-proxy/proxyServer.js';
 import { init, loadConfigurationFile } from './src/main/init.js';
 
-init(process.cwd());
-const config = loadConfigurationFile(process.cwd());
-startGrpcServer(process.cwd(), config);
-startHttpProxy(process.cwd(), config);
+const projectRoot = process.cwd();
+
+init(projectRoot);
+const config = loadConfigurationFile(projectRoot);
+
+if (config?.grpc?.enabled) {
+  startGrpcServer(projectRoot, config);
+}
+
+if (config?.http?.enabled) {
+  startHttpProxy(projectRoot, config);
+}
